5b45c518088f5a57bc5956291899fb92a99042f9
Guilherme Gomes Luccas Rodrigues <118491966+Guilherme-Gomes-Luccas@users.noreply.github.com>
2024-12-11 14:01:18 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.use(cookieParser());
 
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: ['https://back-end-animewave.onrender.com'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
