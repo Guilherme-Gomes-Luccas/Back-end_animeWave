85490eefc12241d167e91d3f553d5bdca1ceecab
Guilherme Gomes Luccas Rodrigues <118491966+Guilherme-Gomes-Luccas@users.noreply.github.com>
2024-12-11 19:23:06 -0300

    Update loginGoogle.ts

--- src/controllers/auth/loginGoogle.ts
+++ src/controllers/auth/loginGoogle.ts
@@ -30,7 +30,6 @@ export class LoginGoogleController {
       secure: false,
       maxAge: 3600000,
       path: '/',
-      domain: 'localhost',
     });
 
     res.cookie('refreshToken', refreshToken, {
@@ -39,9 +38,8 @@ export class LoginGoogleController {
       secure: false,
       maxAge: 3600000,
       path: '/',
-      domain: 'localhost',
     });
 
-    res.status(200).redirect('https://back-end-animewave.onrender.com/home');
+    res.status(200).redirect('https://front-end-anime-wave.vercel.app/home');
   }
 }
